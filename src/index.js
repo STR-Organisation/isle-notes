c98ac0b294727da6df09f9fe0d4417a08cb0ba2b
Advay Patil <advaypatil27@gmail.com>
2022-11-18 23:22:40 -0800

    start on profile page

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
-import {
-  ChakraProvider,
-  ColorModeScript,
-  theme as chakraTheme,
-} from '@chakra-ui/react';
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 import React, { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import App from './App';
 import theme from './theme';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ProfilePage from './routes/ProfilePage';
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
@@ -17,12 +14,16 @@ const router = createBrowserRouter([
     path: '/',
     element: <App />,
   },
+  {
+    path: '/profile',
+    element: <ProfilePage />,
+  },
 ]);
 
 root.render(
   <StrictMode>
     <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-    <ChakraProvider theme={chakraTheme}>
+    <ChakraProvider theme={theme}>
       <RouterProvider router={router} />
     </ChakraProvider>
   </StrictMode>
