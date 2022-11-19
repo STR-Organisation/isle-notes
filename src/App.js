1de3eacc5f2ac986fcfa44a602bea55290d1c12f
Advay Patil <advaypatil27@gmail.com>
2022-11-18 22:10:25 -0800

    integrate navbar + firebase

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { Button } from '@chakra-ui/react';
 import React from 'react';
+import { Navbar } from './components/Navbar';
+import { signInWithGoogle } from './firebase-config';
 
 function App() {
   return (
     <>
-      <div>App</div>
+      <Navbar />
+      <Button colorScheme={'red'} onClick={signInWithGoogle}>
+        Sign In With Google
+      </Button>
     </>
   );
 }
