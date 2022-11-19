3ff4c9921c5a35891e974d36e9b315a36585c0cf
Advay Patil <advaypatil27@gmail.com>
2022-11-18 22:10:10 -0800

    add react-router

--- src/index.js
+++ src/index.js
@@ -7,15 +7,23 @@ import React, { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import App from './App';
 import theme from './theme';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+]);
 
 root.render(
   <StrictMode>
     <ColorModeScript initialColorMode={theme.config.initialColorMode} />
     <ChakraProvider theme={chakraTheme}>
-      <App />
+      <RouterProvider router={router} />
     </ChakraProvider>
   </StrictMode>
 );
