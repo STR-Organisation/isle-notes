7ac38d7cf4ebcda61391881bd7fa33b47959e48a
Advay Patil <advaypatil27@gmail.com>
2022-11-19 15:43:36 -0800

    start on profile page section

--- src/routes/ProfilePage.jsx
+++ src/routes/ProfilePage.jsx
@@ -1,3 +1,71 @@
-import React from 'react';
+import {
+  HStack,
+  Flex,
+  Heading,
+  VStack,
+  Input,
+  Text,
+  Divider,
+} from '@chakra-ui/react';
+import React, { useState } from 'react';
+import CustomCheckbox from '../components/CustomCheckbox';
+import { Navbar } from '../components/Navbar';
+import { auth } from '../firebase-config';
 
-export default function ProfilePage() {}
+export default function ProfilePage() {
+  const [classes, setClasses] = useState([]);
+
+  return (
+    <>
+      <Navbar />
+      <Flex w="100%" align={'center'} flexDir="column">
+        <Heading fontSize={'3vw'}>
+          Welcome, {auth.currentUser.displayName}!
+        </Heading>
+
+        <Divider w={'40vw'} mt={2} />
+
+        <VStack align={'flex-start'} mt={2} w={'40vw'}>
+          <Heading fontSize={'2vw'}>Basic Info</Heading>
+
+          <HStack>
+            <Text fontSize={'1.5vw'} w={'10ch'}>
+              First Name{' '}
+            </Text>
+            <Input
+              maxW={'20vw'}
+              size="sm"
+              fontSize={'1.5vw'}
+              placeholder="Name"
+            />
+          </HStack>
+          <HStack>
+            <Text fontSize={'1.5vw'} w={'10ch'}>
+              Last Name{' '}
+            </Text>
+            <Input
+              maxW={'20vw'}
+              size="sm"
+              fontSize={'1.5vw'}
+              placeholder="Name"
+            />
+          </HStack>
+          <HStack>
+            <Text fontSize={'1.5vw'} w={'10ch'}>
+              Email{' '}
+            </Text>
+            <Input
+              maxW={'20vw'}
+              size="sm"
+              fontSize={'1.5vw'}
+              value={auth.currentUser.email}
+              disabled={true}
+            />
+          </HStack>
+        </VStack>
+
+        <Divider w={'40vw'} mt={2} />
+      </Flex>
+    </>
+  );
+}
