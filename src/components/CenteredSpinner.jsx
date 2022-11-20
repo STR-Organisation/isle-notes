3c2f72f2e08e39bc45fbd6f1fd1262fc3d04bf2d
Advay Patil <advaypatil27@gmail.com>
2022-11-19 22:12:42 -0800

    spinner component for utility purposes

---
+++ src/components/CenteredSpinner.jsx
@@ -0,0 +1,12 @@
+import { Flex, Spinner } from '@chakra-ui/react';
+import React from 'react';
+
+export default function CenteredSpinner() {
+  return (
+    <>
+      <Flex w="100%" justify={'center'}>
+        <Spinner color="tomato" />
+      </Flex>
+    </>
+  );
+}
