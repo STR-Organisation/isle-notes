0cbdfe9e3ff5f607ed99f56e1279eec5343bee16
Advay Patil <advaypatil27@gmail.com>
2022-11-19 19:41:14 -0800

    add subject routes for notes section

--- src/utils.js
+++ src/utils.js
@@ -4,3 +4,31 @@ export const getFirstName = displayName => {
 export const getLastName = displayName => {
   return displayName.split(' ')[1];
 };
+
+export const ALL_SUBJECT_ROUTES = {
+  'IB Economics SL': 'econ-sl',
+  'IB TOK': 'tok',
+  'IB History HL 1': 'hoa',
+  'IB ESS SL': 'ess',
+  'IB Chemistry SL': 'chem',
+  'IB Math A&A HL': 'math-hl',
+  'IB Pers. & Prof. Skills': 'pers-prof',
+  'IB Spanish SL/HL': 'spanish',
+  'IB French SL/HL': 'french',
+  'IB CS SL/HL': 'cs',
+  'IB Theatre Arts SL/HL': 'theatre',
+  'IB Film SL': 'film',
+  'IB Psychology SL': 'psych',
+  'IB World Religions SL': 'world-religions',
+  'IB History HL 2': 'history-2',
+  'IB Exercise Science SL': 'exercise-science',
+  'IB Physics HL': 'phys',
+  'IB Biology HL': 'bio',
+  'IB Math A&A SL': 'math-sl',
+  'IB Mandarin Chinese SL/HL': 'chinese',
+  'IB Japanese SL/HL': 'japanese',
+  'IB Lang. and Lit. HL': 'lang-lit',
+  'IB Business SL/HL': 'business',
+  'IB Visual Arts SL/HL': 'visual-arts',
+  'IB Music SL': 'music',
+};
