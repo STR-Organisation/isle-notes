c97f7d69f3aa04737a5ac84eb2bd6f1b4906e13a
Advay Patil <advaypatil27@gmail.com>
2022-11-19 22:13:12 -0800

    subject display component for /notes page

---
+++ src/components/SubjectDisplay.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import { Flex, Heading, Box } from '@chakra-ui/react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth, db } from '../firebase-config';
+import { query, getDocs, where, collection } from 'firebase/firestore';
+import CenteredSpinner from './CenteredSpinner';
+
+export default function SubjectDisplay({ title, image }) {
+  return (
+    <>
+      <Flex
+        bgImage={image}
+        bgPosition="center"
+        w="sm"
+        height={'2xs'}
+        backgroundSize="cover"
+        backgroundRepeat={'no-repeat'}
+        m={5}
+        borderRadius={'2vw'}
+        align="flex-end"
+        cursor={'pointer'}
+      >
+        <Flex
+          bg={
+            'linear-gradient(to bottom, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 1) 100%)'
+          }
+          w={'100%'}
+          h={'40'}
+          align="flex-end"
+          borderRadius={'2vw'}
+        >
+          <Heading mb={4} ml={4} color={'white'}>
+            {title}
+          </Heading>
+        </Flex>
+      </Flex>
+    </>
+  );
+}
