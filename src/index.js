bc50853a293568031e5ec3679a833dee0bff0e4a
Advay Patil <advaypatil27@gmail.com>
2022-11-20 17:09:36 -0800

    add dynamic routing for individual subject notes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 import React, { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
-import App from './App';
-import theme from './theme';
+import App from './App.js';
+import theme from './theme.js';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import ProfilePage from './routes/ProfilePage';
 import Notes from './routes/Notes';
+import SubjectNotes from './routes/SubjectNotes';
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
@@ -22,6 +23,10 @@ const router = createBrowserRouter([
   {
     path: '/notes',
     element: <Notes />,
+  },
+  {
+    path: '/notes/:subject',
+    element: <SubjectNotes />,
   },
 ]);
 
