561b2292f9af460becb769c8e027daf3055fc6e1
Advay Patil <advaypatil27@gmail.com>
2022-11-22 22:17:41 -0800

    update layout of subject page

--- src/routes/SubjectNotes.jsx
+++ src/routes/SubjectNotes.jsx
@@ -1,4 +1,14 @@
-import { Flex, Heading, Box } from '@chakra-ui/react';
+import {
+  Flex,
+  Heading,
+  Box,
+  Grid,
+  Button,
+  useDisclosure,
+  Drawer,
+  DrawerContent,
+  DrawerFooter,
+} from '@chakra-ui/react';
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { Navbar } from '../components/Navbar';
@@ -27,20 +37,20 @@ export default function SubjectNotes() {
   return (
     <>
       <Navbar />
-      <Flex w="100%" justify={'center'}>
-        <Heading>{fullName}</Heading>
-      </Flex>
       <Flex
         borderRight="1px"
         borderRightColor={'gray.200'}
         w={{ base: 'full', md: 60 }}
-        pos="fixed"
-        h="full"
+        pos="static"
+        minH={'100vh'}
         borderTop={'1px'}
         borderTopColor={'gray.200'}
         align="center"
         flexDir={'column'}
       >
+        <Heading mt={4} fontSize={'xl'} cursor="pointer">
+          {fullName}
+        </Heading>
         {sampleTopics.map((e, idx) => {
           return <TopicText key={idx}>{e}</TopicText>;
         })}
