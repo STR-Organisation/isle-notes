142fb091709854188d7791d24b357b66b9fe6bdd
Advay Patil <advaypatil27@gmail.com>
2022-11-22 22:17:55 -0800

    make topic text code more concise

--- src/components/TopicText.jsx
+++ src/components/TopicText.jsx
@@ -8,10 +8,9 @@ export default function TopicText({ children }) {
       fontSize={'lg'}
       color="black"
       fontWeight={'semibold'}
+      mx="4"
+      p={4}
       mt={4}
-      paddingBlock="4"
-      paddingLeft={4}
-      paddingRight={4}
       _hover={{
         bg: 'tomato',
         color: 'white',
