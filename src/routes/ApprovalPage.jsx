da5f9a461363dcae98756546bf2dd0210ff4c0a0
Advay Patil <advaypatil27@gmail.com>
2022-11-24 14:18:29 -0800

    redirect user if they are not an organizer

--- src/routes/ApprovalPage.jsx
+++ src/routes/ApprovalPage.jsx
@@ -1,10 +1,45 @@
-import React from 'react';
+import { Heading } from '@chakra-ui/react';
+import { collection, query, where } from 'firebase/firestore';
+import React, { useEffect } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
 import { Navbar } from '../components/Navbar';
+import { auth, db } from '../firebase-config';
+import CenteredSpinner from '../components/CenteredSpinner';
+import { useNavigate } from 'react-router-dom';
 
 export default function ApprovalPage() {
+  const organizerTableRef = collection(db, 'organizers');
+
+  const [user] = useAuthState(auth);
+  const navigate = useNavigate();
+
+  let q;
+  if (user) {
+    q = query(organizerTableRef, where('uid', '==', user.uid));
+  }
+  const [organizerInfo] = useCollectionData(q, {
+    idField: 'id',
+  });
+
+  useEffect(() => {
+    console.log(organizerInfo);
+
+    if (organizerInfo) {
+      if (!organizerInfo[0].isOrganizer) {
+        navigate('/');
+      }
+    }
+  }, [organizerInfo]);
+
   return (
     <>
       <Navbar />
+      {organizerInfo ? (
+        <Heading>{`${organizerInfo[0].isOrganizer}`}</Heading>
+      ) : (
+        <CenteredSpinner />
+      )}
     </>
   );
 }
