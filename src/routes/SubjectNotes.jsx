4e1ca3fdaa3e01c46d543e214e3cd0b002908df0
Advay Patil <advaypatil27@gmail.com>
2022-11-23 18:22:57 -0800

    add support for markdown styles

--- src/routes/SubjectNotes.jsx
+++ src/routes/SubjectNotes.jsx
@@ -1,21 +1,12 @@
-import {
-  Flex,
-  Heading,
-  Box,
-  Grid,
-  Button,
-  useDisclosure,
-  Drawer,
-  DrawerContent,
-  DrawerFooter,
-} from '@chakra-ui/react';
+import { Flex, Heading } from '@chakra-ui/react';
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { Navbar } from '../components/Navbar';
 import { getKeyByValue, SUBJECT_SHORTHAND } from '../utils';
 import ReactMarkdown from 'react-markdown';
-import remarkGfm from 'remark-gfm';
 import TopicText from '../components/TopicText';
+import remarkGfm from 'remark-gfm';
+import './SubjectNotes.css';
 
 export default function SubjectNotes() {
   const { subject } = useParams();
@@ -23,9 +14,10 @@ export default function SubjectNotes() {
   const fullName = getKeyByValue(SUBJECT_SHORTHAND, subject);
 
   const markdown = `
-  # **Topic 1**
+  # Topic 1
   This is *some* **markdown**.
   ~~Welcome~~ to [https://reactjs.com](https://google.com)
+  $x^2$
   `;
 
   const sampleTopics = [
@@ -37,23 +29,28 @@ export default function SubjectNotes() {
   return (
     <>
       <Navbar />
-      <Flex
-        borderRight="1px"
-        borderRightColor={'gray.200'}
-        w={{ base: 'full', md: 60 }}
-        pos="static"
-        minH={'100vh'}
-        borderTop={'1px'}
-        borderTopColor={'gray.200'}
-        align="center"
-        flexDir={'column'}
-      >
-        <Heading mt={4} fontSize={'xl'} cursor="pointer">
-          {fullName}
-        </Heading>
-        {sampleTopics.map((e, idx) => {
-          return <TopicText key={idx}>{e}</TopicText>;
-        })}
+      <Flex overflowX={'hidden'}>
+        <Flex
+          borderRight="1px"
+          borderRightColor={'gray.200'}
+          w={{ base: 'full', md: 60 }}
+          pos="static"
+          minH={'100vh'}
+          borderTop={'1px'}
+          borderTopColor={'gray.200'}
+          align="center"
+          flexDir={'column'}
+        >
+          <Heading mt={4} fontSize={'xl'} cursor="pointer">
+            {fullName}
+          </Heading>
+          {sampleTopics.map((e, idx) => {
+            return <TopicText key={idx}>{e}</TopicText>;
+          })}
+        </Flex>
+        <div class="main">
+          <ReactMarkdown children={markdown} remarkPlugins={[remarkGfm]} />
+        </div>
       </Flex>
     </>
   );
