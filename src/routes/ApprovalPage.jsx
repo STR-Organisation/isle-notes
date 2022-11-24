e8e6236665d081fef48976b17650f773d67e5950
Advay Patil <advaypatil27@gmail.com>
2022-11-24 14:33:32 -0800

    add loading of proposals

--- src/routes/ApprovalPage.jsx
+++ src/routes/ApprovalPage.jsx
@@ -1,4 +1,4 @@
-import { Heading } from '@chakra-ui/react';
+import { Heading, Flex, Button } from '@chakra-ui/react';
 import { collection, query, where } from 'firebase/firestore';
 import React, { useEffect } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
@@ -7,24 +7,30 @@ import { Navbar } from '../components/Navbar';
 import { auth, db } from '../firebase-config';
 import CenteredSpinner from '../components/CenteredSpinner';
 import { useNavigate } from 'react-router-dom';
+import { getKeyByValue, SUBJECT_SHORTHAND } from '../utils';
+import Markdown from '../components/Markdown';
 
 export default function ApprovalPage() {
   const organizerTableRef = collection(db, 'organizers');
+  const proposalTableRef = collection(db, 'proposals');
 
   const [user] = useAuthState(auth);
   const navigate = useNavigate();
 
-  let q;
+  let organizerQuery, proposalsQuery;
   if (user) {
-    q = query(organizerTableRef, where('uid', '==', user.uid));
+    organizerQuery = query(organizerTableRef, where('uid', '==', user.uid));
+    proposalsQuery = query(proposalTableRef, where('isApproved', '==', false));
   }
-  const [organizerInfo] = useCollectionData(q, {
+  const [organizerInfo] = useCollectionData(organizerQuery, {
+    idField: 'id',
+  });
+
+  const [proposalInfo] = useCollectionData(proposalsQuery, {
     idField: 'id',
   });
 
   useEffect(() => {
-    console.log(organizerInfo);
-
     if (organizerInfo) {
       if (!organizerInfo[0].isOrganizer) {
         navigate('/');
@@ -32,11 +38,39 @@ export default function ApprovalPage() {
     }
   }, [organizerInfo]);
 
+  useEffect(() => {
+    if (proposalInfo) {
+      console.log(proposalInfo);
+    }
+  }, [proposalInfo]);
+
   return (
     <>
       <Navbar />
       {organizerInfo ? (
-        <Heading>{`${organizerInfo[0].isOrganizer}`}</Heading>
+        <Flex flexDir={'column'} w="100%" align={'center'}>
+          {proposalInfo.map((v, idx) => {
+            return (
+              <>
+                <Flex
+                  flexDir={'column'}
+                  key={idx}
+                  border="1px"
+                  p={4}
+                  marginBlock={2}
+                  borderColor="gray.200"
+                  borderRadius={'lg'}
+                >
+                  <Heading fontSize={'xl'}>
+                    {getKeyByValue(SUBJECT_SHORTHAND, v.className)}: {v.topic}
+                  </Heading>
+                  <Markdown children={v.note} />
+                  <Button colorScheme={'red'}>Approve</Button>
+                </Flex>
+              </>
+            );
+          })}
+        </Flex>
       ) : (
         <CenteredSpinner />
       )}
