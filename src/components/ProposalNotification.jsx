4c8e560f7aad938a2d805019601cd5b2b13432cb
Advay Patil <advaypatil27@gmail.com>
2022-11-24 22:03:35 -0800

    add icon to proposal notification

--- src/components/ProposalNotification.jsx
+++ src/components/ProposalNotification.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import { Divider, Flex, Text } from '@chakra-ui/react';
 import NotificationBadge from '../components/NotificationBadge';
+import { CloseIcon } from '@chakra-ui/icons';
 
-export default function ProposalNotification({ title, className, status }) {
+export default function ProposalNotification({
+  title,
+  className,
+  status,
+  onClose,
+}) {
   return (
     <>
-      <Flex p={4} flexDir="column">
+      <Flex p={4} flexDir="column" position={'relative'}>
+        <CloseIcon
+          position={'absolute'}
+          top={'15%'}
+          left={'80%'}
+          fontSize={'x-small'}
+          cursor="pointer"
+          onClick={onClose}
+        />
         <Text>{title}</Text>
         <Text fontSize={'13'} color="gray.500">
           {className}
