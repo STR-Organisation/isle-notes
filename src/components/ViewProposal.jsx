f8c13a8508d4bbca3fa84b0bf98409cd5fb03ccd
Advay Patil <advaypatil27@gmail.com>
2022-11-26 12:14:34 -0800

    initialize all proposal page ui

---
+++ src/components/ViewProposal.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import {
+  Divider,
+  Flex,
+  HStack,
+  Button,
+  Text,
+  Box,
+  Mark,
+} from '@chakra-ui/react';
+import NotificationBadge from './NotificationBadge';
+import { getKeyByValue, SUBJECT_SHORTHAND } from '../utils';
+import { EditIcon } from '@chakra-ui/icons';
+import { BsEye, BsTrash } from 'react-icons/bs';
+
+export default function ViewProposal({ proposal }) {
+  const { status, className, topic, id } = proposal;
+
+  return (
+    <>
+      <Flex
+        w="45ch"
+        h={'13vh'}
+        border="1px"
+        borderColor={'gray.200'}
+        m={2}
+        borderRadius="lg"
+        p={3}
+        cursor="pointer"
+        flexDir={'column'}
+      >
+        <Text fontSize={'md'} fontWeight="semibold">
+          {topic}
+          <NotificationBadge ml={1} mb={1} text={status} />
+        </Text>
+        <Text fontSize={'sm'} color="gray.500">
+          {getKeyByValue(SUBJECT_SHORTHAND, className)}
+        </Text>
+        <Divider />
+        <Flex flexDir={'column'} align="center" h="100%">
+          <HStack mt={3}>
+            <Button size="sm" leftIcon={<BsEye />}>
+              View
+            </Button>
+            <Button size={'sm'} leftIcon={<EditIcon />}>
+              Edit
+            </Button>
+            <Button size="sm" leftIcon={<BsTrash />}>
+              Delete
+            </Button>
+          </HStack>
+        </Flex>
+      </Flex>
+    </>
+  );
+}
--- src/routes/AllProposals.jsx
+++ src/routes/AllProposals.jsx
@@ -1,10 +1,44 @@
-import React from 'react';
+import { Flex } from '@chakra-ui/react';
+import { collection, getDocs, query, where } from 'firebase/firestore';
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import CenteredSpinner from '../components/CenteredSpinner';
 import { Navbar } from '../components/Navbar';
+import ViewProposal from '../components/ViewProposal';
+import { auth, db } from '../firebase-config';
 
 export default function AllProposals() {
+  const proposalRef = collection(db, 'proposals');
+
+  const [user] = useAuthState(auth);
+  const [proposals, setProposals] = useState();
+
+  let q;
+  if (user) {
+    q = query(proposalRef, where('uid', '==', user.uid));
+  }
+
+  useEffect(() => {
+    const fetchProposals = async () => {
+      const data = await getDocs(q);
+      setProposals(data.docs.map(v => ({ ...v.data(), id: v.id })));
+    };
+    fetchProposals();
+    console.log(proposals);
+  }, []);
+
   return (
     <>
       <Navbar />
+      {proposals ? (
+        <Flex h="full" flexWrap={'wrap'}>
+          {proposals.map((v, idx) => (
+            <ViewProposal proposal={v} />
+          ))}
+        </Flex>
+      ) : (
+        <CenteredSpinner />
+      )}
     </>
   );
 }
