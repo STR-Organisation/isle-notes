316506b42eeafe21335b05151370984bb626adc3
Advay Patil <advaypatil27@gmail.com>
2022-11-26 11:25:15 -0800

    more animations!

--- src/components/ProposalNotification.jsx
+++ src/components/ProposalNotification.jsx
@@ -19,13 +19,32 @@ export default function ProposalNotification({
         flexDir="column"
         position={'relative'}
         cursor="pointer"
+        _hover={{
+          bgColor: 'gray.50',
+        }}
+        transition="all 200ms ease"
         {...props}
       >
         <HStack position={'absolute'} top={'15%'} left={'80%'}>
           <RouterLink to={to}>
-            <EditIcon fontSize={'sm'} mb={1} />
+            <EditIcon
+              fontSize={'sm'}
+              mb={1}
+              _hover={{
+                transform: 'scale(1.2)',
+              }}
+              transition="all 200ms ease"
+            />
           </RouterLink>
-          <CloseIcon fontSize={'x-small'} cursor="pointer" onClick={onClose} />
+          <CloseIcon
+            fontSize={'x-small'}
+            cursor="pointer"
+            onClick={onClose}
+            _hover={{
+              transform: 'scale(1.2)',
+            }}
+            transition="all 200ms ease"
+          />
         </HStack>
         <Text maxW="17ch">{title}</Text>
         <Text fontSize={'13'} color="gray.500">
