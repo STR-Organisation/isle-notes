e639eb07fd75f9c1846de285695c4ec98c4b76f3
Advay Patil <advaypatil27@gmail.com>
2022-11-25 21:42:58 -0800

    add syntax highlighting to markdown editor

--- src/components/Markdown.jsx
+++ src/components/Markdown.jsx
@@ -5,6 +5,8 @@ import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 import './Markdown.css';
 import 'katex/dist/katex.min.css';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { oneLight } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 export default function Markdown({ children }) {
   return (
@@ -14,6 +16,24 @@ export default function Markdown({ children }) {
           remarkPlugins={[remarkGfm, remarkMath]}
           rehypePlugins={rehypeKatex}
           children={children}
+          components={{
+            code({ node, inline, className, children, ...props }) {
+              const match = /language-(\w+)/.exec(className || '');
+              return !inline && match ? (
+                <SyntaxHighlighter
+                  children={String(children).replace(/\n$/, '')}
+                  style={oneLight}
+                  language={match[1]}
+                  PreTag="div"
+                  {...props}
+                />
+              ) : (
+                <code className={className} {...props}>
+                  {children}
+                </code>
+              );
+            },
+          }}
         />
       </div>
     </>
