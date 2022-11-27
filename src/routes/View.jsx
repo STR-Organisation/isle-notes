f7ce43e1b2a070d2702f8746e81ac047a4e3235b
Advay Patil <advaypatil27@gmail.com>
2022-11-26 18:38:08 -0800

    add view page

--- src/routes/View.jsx
+++ src/routes/View.jsx
@@ -1,15 +1,69 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
 import { Navbar } from '../components/Navbar';
-import { Heading } from '@chakra-ui/react';
+import { Box } from '@chakra-ui/react';
+import {
+  doc,
+  getDoc,
+  query,
+  getDocs,
+  where,
+  collection,
+} from 'firebase/firestore';
+import { db } from '../firebase-config';
+import { auth } from '../firebase-config';
+import Markdown from '../components/Markdown';
 
 export default function View() {
   const { id } = useParams();
 
+  const [note, setNote] = useState();
+
+  const navigate = useNavigate();
+
+  const organizerRef = collection(db, 'organizers');
+
+  useEffect(() => {
+    const fetchDoc = async () => {
+      const docRef = doc(db, 'proposals', id);
+      const docSnap = await getDoc(docRef);
+
+      if (!docSnap.exists()) {
+        navigate('/');
+      }
+
+      const organizerQuery = query(
+        organizerRef,
+        where('uid', '==', auth.currentUser.uid)
+      );
+      const organizerData = await getDocs(organizerQuery);
+
+      if (organizerData.docs[0].data().isOrganizer) {
+        console.log('organizer');
+        setNote(docSnap.data().note);
+        return;
+      }
+
+      if (docSnap.data().uid !== auth.currentUser.uid) {
+        navigate('/');
+      }
+
+      setNote(docSnap.data().note);
+    };
+    fetchDoc();
+  }, []);
   return (
     <>
       <Navbar />
-      <Heading>{id}</Heading>
+      <Box
+        w="100%"
+        h="93vh"
+        bg={'gray.50'}
+        borderRadius="none"
+        borderTop={'none'}
+      >
+        <Markdown>{note}</Markdown>
+      </Box>
     </>
   );
 }
